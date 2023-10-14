8c2d250939ce22844cc020cc332cea78d91a1a0d
Taha Zubeyir TURGUT <tahazubeyir.turgut@student.ucll.be>
2023-10-14 17:10:56 +0200

    project init

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import random
+
+def print_random_int(numb):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'random int: {numb}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    numb = random.randint(1,100);
+    print_random_int(numb)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
